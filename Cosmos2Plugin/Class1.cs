8fc6720b554a620fc03631588534c600b9360059
Tsuyoshi Ushio <ushio@simplearchitect.com>
2022-06-20 20:48:02 -0700

    Add project files.

---
+++ Cosmos2Plugin/Class1.cs
@@ -0,0 +1,16 @@
+﻿using PluginBase;
+
+namespace CosmosPlugin
+{
+    public class CosmosDBPlugin : ICommand
+    {
+        public string Name => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public int Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PluginBase/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PluginBase
+{
+    public class Class1
+    {
+
+    }
+}
